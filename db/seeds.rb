885198cd98b71989001623422fa44b7cb8375fc4
chiragdev09 <chirag@tapitechinc.com>
2022-04-04 03:01:20 +0530

    Seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+BookingDetail.all.destroy_all
 Slot.all.destroy_all
 EntryPoint.all.destroy_all
 Location.all.destroy_all
